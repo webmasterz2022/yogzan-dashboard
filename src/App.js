d2d407ea96158d150a6cc60452af834094a96023
telkomdev-fadlulazmi <fadlulazmi17@gmail.com>
2022-08-26 13:46:03 +0700

    change web title and description

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          COMING SOON
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          YOGZAN DASHBOARD
         </a>
       </header>
     </div>
