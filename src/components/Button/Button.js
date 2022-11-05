9c70b86e3b33bfc210afe38f39b60cc00077981c
telkomdev-fadlulazmi <fadlulazmi17@gmail.com>
2022-11-05 21:47:06 +0800

    add state isLoading

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,13 +2,13 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export default function Button(props) {
-  const {icon, children, className, variant, handleClick, type, disabled} = props
+  const {icon, children, className, variant, handleClick, type, disabled, isLoading} = props
   const classname = [styles.root, styles[variant], className].join(' ')
 
   return (
     <button disabled={disabled} className={classname} onClick={handleClick} type={type || 'button'}>
       {icon && <img className={styles.icon} src={icon} />}
-      <p>{children}</p>
+      <p>{children}{isLoading && '...'}</p>
     </button>
   )
 }
