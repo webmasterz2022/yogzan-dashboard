b75e8b2438d1734cbf7c67dd52255a79a7d86762
Fadlul Azmi <fadlulazmi@Fadluls-MacBook-Air.local>
2025-11-18 15:21:00 +0700

    chore: add vercel analytics

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './store'
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
+    <Analytics />
   </React.StrictMode>
 );
 
